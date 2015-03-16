7b19a88af13a7569d3545b710ba3230a253faa7e
Andrii Pylat <pilatao91@gmail.com>
2015-03-16 16:20:13 +0200

    Added modules array to config. Improved basicLayout to render modules due to config file. Replaced module  wrappers from basicLayout to aproppriate module files. Fixed and improved on scroll function. Improved styles for menu. Created footer module and aproppriate scss file. Created Overview module.

--- app/scripts/modules/BasicLayout.js
+++ app/scripts/modules/BasicLayout.js
@@ -34,50 +34,33 @@ var modulesToRender = configModules.map(function(item) {
 });
 
 var Registration = require('./Registration');
+var Footer = require('./Footer');
+var Overview = require('./Overview');
 var config = require('../config');
 
-var menuItems = [];
-
-var Modules = React.createClass({
-  getInitialState: function() {
-    return {
-      moduleFiles: {
-        location: <LocationMap />,
-        speakers: <Speakers />,
-        partners: <Partners />,
-        schedule: <Schedule />
-      }
-    }
-  },
-
-  render: function() {
-    if (this.props.data.isRendering) {
-      menuItems.push(this.props.data.title);
-      return (
-        <section id={this.props.data.title} className="page-wrap">
-          <h2 className="module-header">{this.props.data.title}</h2>
-          {this.state.moduleFiles[this.props.data.title]}
-        </section>
-      );
-    } else {
-      return <div className="rejected-module"></div>
-    }
+var configModules = [];
+config.modules.map(function(item) {
+  if (item.isRendering) {
+    return configModules.splice(item.order, 0, item.title);
+  } else {
+    return;
   }
 });
 
+var reactModules = {
+  overview: <Overview />,
+  speakers: <Speakers />,
+  schedule: <Schedule />,
+  location: <LocationMap />,
+  partners: <Partners />,
+  footer: <Footer />
+};
+var modulesToRender = configModules.map(function(item) {
+  return reactModules[item];
+});
+
 var LayoutBasic = React.createClass({
-    getInitialState: function() {
-    return {
-      modules: config.modules,
-      menuItems: menuItems
-    }
-  },
-
   render: function() {
-    var modulesToRender = this.state.modules.map(function(module) {
-      return <Modules key={module.title} data={module} />
-    });
-
     return (
       <div className="page-wrap">
         <Header />
--- app/scripts/modules/Menu.js
+++ app/scripts/modules/Menu.js
@@ -51,12 +51,6 @@ var Menu = React.createClass({
   }
 });
 
-//var Item = React.createClass({
-//  render: function() {
-//    return <a href={'#' + this.props.text} className="menu__item" onClick={this.menuLinkHandler}>{this.props.text}</a>
-//  }
-//});
-
 //fix menu when scrolling os make static due to window.pageYOffset
 window.onscroll = function() {
   var menu = document.getElementById('menu');
