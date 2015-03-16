dd56cf5b6e849e7af291b000cf8fdf1b75fa1eac
Andrii Pylat <pilatao91@gmail.com>
2015-03-16 17:56:34 +0200

    replaced section wrapper from basicLayout to registration module.

--- app/scripts/modules/BasicLayout.js
+++ app/scripts/modules/BasicLayout.js
@@ -5,16 +5,10 @@ var React = require('react');
 var Header = require('./Header');
 var Menu = require('./Menu');
 var LocationMap = require('./LocationMap');
-<<<<<<< HEAD
 var Speakers = require('./Speakers.jsx');
 var Partners = require('./Partners.js');
 var Schedule = require('./Schedule.jsx');
 var Registration = require('./Registration.jsx');
-=======
-var Speakers = require('./Speakers');
-var Partners = require('./Partners');
-var Schedule = require('./Schedule');
->>>>>>> Added modules array to config. Improved basicLayout to render modules due to config file. Replaced module  wrappers from basicLayout to aproppriate module files. Fixed and improved on scroll function. Improved styles for menu. Created footer module and aproppriate scss file. Created Overview module.
 var Footer = require('./Footer');
 var Overview = require('./Overview');
 var config = require('../config');
@@ -30,10 +24,7 @@ config.modules.map(function(item) {
 
 var reactModules = {
   overview: <Overview />,
-<<<<<<< HEAD
   registration: <Registration />,
-=======
->>>>>>> Added modules array to config. Improved basicLayout to render modules due to config file. Replaced module  wrappers from basicLayout to aproppriate module files. Fixed and improved on scroll function. Improved styles for menu. Created footer module and aproppriate scss file. Created Overview module.
   speakers: <Speakers />,
   schedule: <Schedule />,
   location: <LocationMap />,
@@ -43,7 +34,6 @@ var reactModules = {
 var modulesToRender = configModules.map(function(item) {
   return reactModules[item];
 });
-<<<<<<< HEAD
 
 var LayoutBasic = React.createClass({
     getInitialState: function() {
@@ -51,10 +41,6 @@ var LayoutBasic = React.createClass({
       modules: config.modules
     }
   },
-=======
->>>>>>> Added modules array to config. Improved basicLayout to render modules due to config file. Replaced module  wrappers from basicLayout to aproppriate module files. Fixed and improved on scroll function. Improved styles for menu. Created footer module and aproppriate scss file. Created Overview module.
-
-var Registration = require('./Registration');
 
 var LayoutBasic = React.createClass({
   render: function() {
@@ -62,11 +48,7 @@ var LayoutBasic = React.createClass({
       <div className="page-wrap">
         <Header />
         <Menu items={configModules} />
-<<<<<<< HEAD
           { modulesToRender }
-        <Footer />
-=======
-        { modulesToRender }
         <Footer />
         // <section id="registration" className="page-wrap">
         // <h2 className="module-header">Registration</h2>
@@ -74,7 +56,6 @@ var LayoutBasic = React.createClass({
         //     here to be inserted registration module
         //   </div>
         // </section>
->>>>>>> Added modules array to config. Improved basicLayout to render modules due to config file. Replaced module  wrappers from basicLayout to aproppriate module files. Fixed and improved on scroll function. Improved styles for menu. Created footer module and aproppriate scss file. Created Overview module.
       </div>
     );
   }
--- app/scripts/modules/Registration.js
+++ app/scripts/modules/Registration.js
@@ -3,18 +3,86 @@ var React = require('react');
 var Registration = React.createClass({
   render: function() {
     return (
-      <form className="registration" role="form" onChange={this.handleChange}>
-        <div>
-            <input className="registration__input registration__input--transition" type="text" name="fName" ref="firstName" placeholder="User Name"/>
+      <section id="registration" className="page-wrap">
+      <h2 className="module-header">Registration</h2>
+        <div className="container">
+          <div className="registration">
+            <RegistrationForm onDataReceived={this.pushData}/>
+          </div>
         </div>
-        <div>
-            <input className="registration__input registration__input--transition" type="text" name="email" ref="email" placeholder="Email Address"/>
-        </div>
-        <div>
-            <input className="registration__input registration__input--transition" type="password" name="password" ref="password" value="" placeholder="Password"/>
-        </div>
-        <div className="submit">
-          <input type="submit" value="Register"/>
+      </section>
+    )
+  }
+});
+
+var RegistrationForm = React.createClass({
+  getInitialState: function() {
+    return ({
+      name:false,
+      email:false,
+      phone:false,
+      nameTipIsShown:false,
+      emailTipIsShown:false,
+      phoneTipIsShown:false,
+      clear:false
+    })
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    this.checkData();
+    if (this.isValid()) {
+      this.props.onDataReceived(this.isValid());
+      this.clearForm();
+    };
+  },
+  checkData: function() {
+    if (!this.state.name) {
+      this.setState({nameTipIsShown: true});
+    };
+    if (!this.state.email) {
+      this.setState({emailTipIsShown: true});
+    };
+    if (!this.state.phone) {
+      this.setState({phoneTipIsShown: true});
+    };
+  },
+  nameIsValid: function(name) {
+    if (name) {
+      this.setState({name:name, nameTipIsShown: false});
+    };
+  },
+  emailIsValid: function(email) {
+    if (email) {
+      this.setState({email:email, emailTipIsShown: false});
+    };
+  },
+  phoneIsValid: function(phone) {
+    if (phone) {
+      this.setState({phone: phone, phoneTipIsShown: false});
+    };
+  },
+  isValid: function() {
+    if (this.state.name && this.state.email && this.state.phone) {
+      return {
+        name: this.state.name,
+        email: this.state.email,
+        phone : this.state.phone
+      }
+    } else {
+      return false
+    };
+  },
+  clearForm: function() {
+    this.setState({clear:true});
+  },
+  render: function() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <NameInput valueReceived={this.nameIsValid} tipIsShown={this.state.nameTipIsShown} clear={this.state.clear}/>
+        <EmailInput valueReceived={this.emailIsValid} tipIsShown={this.state.emailTipIsShown} clear={this.state.clear}/>
+        <PhoneInput valueReceived={this.phoneIsValid} tipIsShown={this.state.phoneTipIsShown} clear={this.state.clear}/>
+        <div className="registration__field">
+          <input type="submit" className="registration__submit" value="Register"/>
         </div>
       </form>
     );
